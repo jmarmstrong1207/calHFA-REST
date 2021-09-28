96e9a89bb71823b4d32783bb0ed9fdc5ffe362e8
jmarmstrong1207 <jm.armstrong1207@gmail.com>
2021-09-28 15:37:58 -0700

    Remove example code and add GET example codes relevant to the project

--- CalHFA API/Startup.cs
+++ CalHFA API/Startup.cs
@@ -28,10 +28,6 @@ namespace CalHFA_API
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CalHFA_API", Version = "v1" });
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,8 +36,6 @@ namespace CalHFA_API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CalHFA_API v1"));
             }
 
             app.UseHttpsRedirection();
