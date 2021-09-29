d8250337a9569e7c61292a76903ec7d0586ecc73
jmarmstrong1207 <jm.armstrong1207@gmail.com>
2021-09-28 18:55:09 -0700

    Add async returns

--- CalHFA_API/Controllers/DataController.cs
+++ CalHFA_API/Controllers/DataController.cs
@@ -11,23 +11,24 @@ namespace CalHFA_API.Controllers
     [Route("api/[controller]")]
     public class DataController : Controller
     {
+        // ALL OF THESE ARE ASYNC. PUT AWAIT KEYWORD BEFORE LONG CALLS LIKE ACCESSING DATABASE TO INCREASE ASYNCHRONOUS ABILITIES
 
         [HttpGet]
-        public string Index()
+        public async Task<string> Index()
         {
             return "This is the index";
         }
 
-        // GET: api/data/*string to put
+        // GET: api/data/*string to put*
         [HttpGet("{str}")]
-        public string GetString(string str)
+        public async Task<string> GetString(string str)
         {
             return str;
         }
 
         // GET: api/data/Details/5
         [HttpGet("details/{id}")]
-        public int Details(int id)
+        public async Task<int> Details(int id)
         {
             return id;
         }
