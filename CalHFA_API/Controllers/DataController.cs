5d0da400d2313daa450ec9bf8db3e6e2e0489447
jmarmstrong1207 <jm.armstrong1207@gmail.com>
2021-10-05 11:45:49 -0700

    Remove async

--- CalHFA_API/Controllers/DataController.cs
+++ CalHFA_API/Controllers/DataController.cs
@@ -14,21 +14,21 @@ namespace CalHFA_API.Controllers
         // ALL OF THESE ARE ASYNC. PUT AWAIT KEYWORD BEFORE LONG CALLS LIKE ACCESSING DATABASE TO INCREASE ASYNCHRONOUS ABILITIES
 
         [HttpGet]
-        public async Task<ActionResult> Index()
+        public ActionResult Index()
         {
             return Ok("This is the index");
         }
 
         // GET: api/data/*string to put*
         [HttpGet("{str}")]
-        public async Task<ActionResult> GetString(string str)
+        public ActionResult GetString(string str)
         {
             return Ok(str);
         }
 
         // GET: api/data/Details/5
         [HttpGet("details/{id}")]
-        public async Task<ActionResult> Details(int id)
+        public ActionResult Details(int id)
         {
             return Ok(id);
         }
