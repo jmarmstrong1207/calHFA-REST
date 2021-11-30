7c5606866dae5c245b36cdc7af64f943844481e9
JVilla1997 <89045411+JVilla1997@users.noreply.github.com>
2021-11-29 19:03:07 -0800

    Features2.0 (#5)
    * added [HttpGet("MyHome/ZIP/MySchool")]
    
    * Connected SQL query to C#. Created a private function that retrieves count of loans and date for MyHome/ZIP/School
    
    * Removed comments at the end of DataControllers, added date loan functions
    
    * Added more functions to display the data
    
    * Added new LoansInLine entity and fix the SQL logic

--- CalHFA_API/Controllers/DataController.cs
+++ CalHFA_API/Controllers/DataController.cs
@@ -1,10 +1,9 @@
-﻿using CalHFA_API.Models;
-using Microsoft.AspNetCore.Http;
+using CalHFA_API.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MySql.Data.MySqlClient;
 
 namespace CalHFA_API.Controllers
 {
@@ -16,30 +15,70 @@ namespace CalHFA_API.Controllers
         [HttpGet("getLoans")]
         public ActionResult getLoans()
         {
-            // Example return
             Loans l = new()
             {
                 ComplianceReviewCount = 1,
+                ComplianceReviewDate = "",
                 ComplianceReviewSuspenseCount = 2,
+                ComplianceReviewSuspenseDate = "",
                 PurchaseReviewCount = 3,
-                PurchaseReviewSuspenseCount = 4
+                PurchaseReviewDate = "",
+                PurchaseReviewSuspenseCount = 4,
+                PurchaseReviewSuspenseDate = "",
             };
+            //compliance loans in line, date and count
+            MyStruct myStruct = loansInLine(410);
+            l.ComplianceReviewCount = myStruct.count;
+            l.ComplianceReviewDate = myStruct.date;
+
+            //compliance loans in suspense, date and count
+            myStruct = loansInLine(422);
+            l.ComplianceReviewSuspenseCount = myStruct.count;
+            l.ComplianceReviewSuspenseDate = myStruct.date;
+
+            //purchase loans in line, date and count
+            myStruct = loansInLine(510);
+            l.PurchaseReviewCount = myStruct.count;
+            l.PurchaseReviewDate = myStruct.date;
+
+            //purchase loans in suspense, date and count
+            myStruct = loansInLine(522);
+            l.PurchaseReviewSuspenseCount = myStruct.count;
+            l.PurchaseReviewSuspenseDate = myStruct.date;
 
             return Ok(l);
         }
-        // GET FOR COMPLIANCE
-        [HttpGet("Compliance")]
-        private ActionResult Compliance(string compliance)
+        //creates a struct that will be in charge of storing the date for the loans and the count of the loans
+        public struct MyStruct
         {
-            return Ok(compliance);
+            public int count;
+            public String date;
         }
-
-        [HttpGet("test")]
-        public ActionResult test()
+        //get function for loans in line as well as the date
+        private MyStruct loansInLine(int statusCode)
         {
-            var x = new loanschemaContext();
-            var loans = x.Loans.ToList();
-            return Ok(loans);
+            MyStruct retVal;
+
+            retVal.count = 0;
+            retVal.date = " ";
+
+            using (var context = new loanschemaContext())
+            {
+                #region FromSqlRaw
+                //query that uses the view created inside the data base
+                var lonsInLine = context.LoansInLines.FromSqlRaw("" +
+                     "select * from LoansInLine " +
+                     "where StatusCode = " + statusCode + " " +
+                     "order by StatusDate desc").ToList();
+
+                if (lonsInLine.Count > 0)
+                {
+                    retVal.count = lonsInLine[0].CountLoans.Value;
+                    retVal.date = lonsInLine[0].StatusDate.ToString();
+                }
+                #endregion
+            }
+            return retVal;
         }
     }
 }
